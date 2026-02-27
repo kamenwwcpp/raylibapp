cef00a24f61a9d222ec074722e0c2cbcdb901898
kamen <voloshin.mn@dvfu.ru>
2026-02-27 14:14:10 +1000

    0.1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "raylib.h"
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Raylib Project - Hello!");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 190, 200, 20, ORANGE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
